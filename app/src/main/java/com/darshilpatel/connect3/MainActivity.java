b356bc4f219a5e22741d6c2a1a4d52952d94c182
darshilpatel <darshil-patel@hotmail.com>
2016-04-16 20:02:20 -0400

    animated player piece dropIn

--- app/src/main/java/com/darshilpatel/connect3/MainActivity.java
+++ app/src/main/java/com/darshilpatel/connect3/MainActivity.java
@@ -1,9 +1,33 @@
 package com.darshilpatel.connect3;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int currentActivePlayer = 0; // 0 = yellow, 1 = red
+
+    public void dropIn (View view) {
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000);
+
+        if (currentActivePlayer == 0 ){
+            counter.setImageResource(R.drawable.yellow);
+            currentActivePlayer = 1;
+        }  else {
+                counter.setImageResource(R.drawable.red);
+                currentActivePlayer = 0;
+            }
+
+
+        counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
