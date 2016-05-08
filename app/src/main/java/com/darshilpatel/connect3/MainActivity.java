62ac04f9e4bf7299e229ad440ff35fd462addc63
darshilpatel <darshil-patel@hotmail.com>
2016-05-08 17:16:30 -0400

    added play again button

--- app/src/main/java/com/darshilpatel/connect3/MainActivity.java
+++ app/src/main/java/com/darshilpatel/connect3/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     int currentActivePlayer = 0; // 0 = yellow, 1 = red
+
+    boolean gameStatus = true;
 
     int [] gameState = {2,2,2,2,2,2,2,2,2}; // 2 = unplayed piece
     int [][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         ImageView counter = (ImageView) view;
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if (gameState[tappedCounter] == 2 ){
+        if (gameState[tappedCounter] == 2  && gameStatus){
             gameState[tappedCounter] = currentActivePlayer;
 
                 counter.setTranslationY(-1000);
@@ -48,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         for (int[] winningPosition : winningPositions){
             if (gameState[winningPosition[0]] ==  gameState[winningPosition[1]] &&
                     gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2){
-
+                gameStatus = false;
                 String winner = "Red";
 
                 if (gameState[winningPosition[0]]== 0 ){
@@ -71,11 +74,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void playAgin (View view){
+    public void playAgain(View view){
+
+        gameStatus = true;
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
 
         layout.setVisibility(View.INVISIBLE);
+
+
+        currentActivePlayer = 0; // 0 = yellow, 1 = red
+
+            for (int i = 0; i < gameState.length; i++){
+                gameState[i] = 2;
+            }
+        GridLayout gridLayout = (GridLayout)findViewById(R.id.gridLayout);
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++){
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+        }
 
     }
 
